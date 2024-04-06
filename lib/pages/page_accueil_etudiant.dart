aebd6deb1314fd887f191cf1c5188e0ae0712765
MahMawd <mahmoudbenmoussa91@gmail.com>
2024-04-06 01:56:59 +0100

    dropdown menu accueil etudiant

--- lib/pages/page_accueil_etudiant.dart
+++ lib/pages/page_accueil_etudiant.dart
@@ -2,9 +2,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:unitransit_app_1/pages/page_authentification.dart';
 
-class HomePage extends StatelessWidget{
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget{
+  HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> stations = ['Tunis Marine', 'Station 2', 'Station 3', 'Station 4']; // Add your station names here
+
+    String selectedStation = 'Tunis Marine'; // Initially select the first station
   @override 
   Widget build(BuildContext context){
     return  Scaffold(
@@ -18,7 +26,6 @@ class HomePage extends StatelessWidget{
         child: SingleChildScrollView(
           child: Column(
             children: [
-          
              Padding(
                padding: const EdgeInsets.symmetric(horizontal: 25.0),
                child: Column(
@@ -63,63 +70,109 @@ class HomePage extends StatelessWidget{
                 ],
                ),
                //bch tba3d choose your placement al box loc
-              const SizedBox(
-                height: 20,
-               ),
+              const SizedBox(height: 20,),
                //box locationn
-                         Container(
+              Container(
+                width: 400,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(12),
                 ),
-                padding: const EdgeInsets.all(12), //el kober mta3 box from
-                // fi west box fih location
-                child: const Row(
-                  children: [
-                    Icon(
+                padding: const EdgeInsets.all(12),
+                    child:Row(
+                      children: <Widget>[
+                        const Icon(
                       Icons.location_on,
                       color: Colors.black),
-                      SizedBox(
-                        width:10,
-                      ),
-                    Text('From     Tunis Marine',
-                    style: TextStyle(
-                      color: Colors.black,
+                     const SizedBox(width:10,),
+                      const Text("From"),
+                      const SizedBox(width:10,),
+                        Container(
+                          height: 60.0,
+                          width: 250.0,
+                          child: ButtonTheme(
+                            alignedDropdown: true,
+                            child: DropdownButtonFormField<String>(
+                              isExpanded: true,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(12.0),)
+                                ),
+                                fillColor: Colors.white,
+                                filled: true,
+                              ),
+                              value: selectedStation,
+                              onChanged: (String? newValue) {
+                                if (newValue != null) {
+                                  setState(() {
+                                    selectedStation = newValue;
+                                    });
+                                  }
+                                },
+                                items: stations.map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                    );
+                                  }).toList(),
+                              ),
+                          ),
+                        ),
+                      ],
                     ),
-                    ),
-                 ],
-                ),
-                         ),
-                       
-                       
-                         const SizedBox(
-                height: 20,
-                         ),
-                       
-                       
-               Container(
+
+              ),
+              const SizedBox(height: 20,),    
+              Container(
+                width: 400,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(12),
                 ),
-                padding: const EdgeInsets.all(12), //el kober mta3 box from
-                // fi west box fih location
-                child: const Row(
-                  children: [              
-                    Icon(
+                padding: const EdgeInsets.all(12),
+                    child:Row(
+                      children: <Widget>[
+                        const Icon(
                       Icons.location_on,
                       color: Colors.black),
-                     SizedBox(
-                        width:10,
-                      ),
-                    Text('To     Faculty of sciences',
-                    style: TextStyle(
-                      color: Colors.black,
+                     const SizedBox(width:10,),
+                      const Text("To"),
+                      const SizedBox(width:25,),
+                        Container(
+                          height: 60.0,
+                          width: 250.0,
+                          child: ButtonTheme(
+                            alignedDropdown: true,
+                            child: DropdownButtonFormField<String>(
+                              isExpanded: true,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(12.0),)
+                                ),
+                                fillColor: Colors.white,
+                                filled: true,
+                              ),
+                              value: selectedStation,
+                              onChanged: (String? newValue) {
+                                if (newValue != null) {
+                                  setState(() {
+                                    selectedStation = newValue;
+                                    });
+                                  }
+                                },
+                                items: stations.map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                    );
+                                  }).toList(),
+                              ),
+                          ),
+                        ),
+                      ],
                     ),
-                    ),
-                 ],
-                ),
-                         ),
+
+              ),
                        
                          const SizedBox(
                 height: 25,
@@ -220,26 +273,24 @@ class HomePage extends StatelessWidget{
                   ],
                 ),
               ),
-                      
-              
             ),
-
-            ElevatedButton(
-                onPressed: () async {
-                  try {
-                    await FirebaseAuth.instance.signOut();
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Authentification())); // Return to the sign-in page
-                  } catch (e) {
-                    print('Failed to sign out: $e');
-                  }
-                },
-                child: const Text('Sign Out'),
-              ),
+            Container(
+              child: ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      await FirebaseAuth.instance.signOut();
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Authentification())); // Return to the sign-in page
+                    } catch (e) {
+                      print('Failed to sign out: $e');
+                    }
+                  },
+                  child: const Text('Sign Out'),
+                ),
+            ),
             ],
           ),
         ),
       )
     );
   }
-
 }
