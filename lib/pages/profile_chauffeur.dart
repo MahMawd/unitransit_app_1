e3796658c20d0ed5c34e207436b741d8d5364a6a
MahMawd <mahmoudbenmoussa91@gmail.com>
2024-04-20 20:04:59 +0100

    added alert to chauff

--- lib/pages/profile_chauffeur.dart
+++ lib/pages/profile_chauffeur.dart
@@ -10,74 +10,92 @@ class ProfileScreenChauffeur extends StatelessWidget{
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text("Profil",
-        style: Theme.of(context).textTheme.headlineMedium ),
-        actions: const [
-
-        ],
-      ),
-      body:SingleChildScrollView(
-        child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-               const SizedBox(
-                  width: 120,
-                  height: 120,
-                  child: ClipRRect(
+      body:SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                  alignment: AlignmentDirectional.topStart,
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Profil",
+                        style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,
+                        ),
+                        ),
+                        Text('Consulter vos données',
+                        style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                const SizedBox(height:10),
                 const SizedBox(height:20),
-                /*SizedBox(
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () => Get.to(() =>  UpdateProfile()), 
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow, side: BorderSide.none, shape: const StadiumBorder(), ),
-                child: const Text("Edit Profile",style: TextStyle(color: Colors.black),),             
+            const SizedBox(
+                width: 180,
+                height: 200,
+                child: Image(image:AssetImage('lib/images/chauffeur-icon.png',),
+                fit: BoxFit.cover,
                 ),
-                ),*/
-                /*StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance.collection('etudiant').snapshots(),
-                 builder: (context,snapshot){
-                  List<Row> etudiantWidgets = [];
-        
-                  if (snapshot.hasData){
-                    final etudiants = snapshot.data?.docs.reversed.toList();
-                    for(var etud in etudiants!){
-                      final etudiantWidget=Row(
-                        children: [
-                          Text(etud['username']),
-                          ],);
-                          etudiantWidgets.add(etudiantWidget);
-                    }
-                  }
-                  return Expanded(
-                    child: ListView(
-                      children: etudiantWidgets,
-                    ),
-                  );
-                 }
-                 ),*/
-                 GetProfile(_uid!,'chauffeur'),
-                 //ProfileMenuWidget(title:"Log out" ,subtitle: 'goodbye',icon:Icons.logout ,onPress: (){},endIcon: false,textColor: Colors.red,),
-                  ElevatedButton(
-                onPressed: () async {
-                  try {
-                    await FirebaseAuth.instance.signOut();
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const Authentification())); // Return to the sign-in currentPage
-                  } catch (e) {
-                    debugPrint('Failed to sign out: $e');
-                  }
-                },
-                child: const Text('Déconnexion'),
               ),
-              ],
+            
+            //  const SizedBox(
+            //     width: 120,
+            //     height: 120,
+            //     child: ClipRRect(
+            //     ),
+            //   ),
+            //   const SizedBox(height:10),
+            //   const SizedBox(height:20),
+              /*SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () => Get.to(() =>  UpdateProfile()), 
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow, side: BorderSide.none, shape: const StadiumBorder(), ),
+              child: const Text("Edit Profile",style: TextStyle(color: Colors.black),),             
+              ),
+              ),*/
+              /*StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection('etudiant').snapshots(),
+               builder: (context,snapshot){
+                List<Row> etudiantWidgets = [];
+                    
+                if (snapshot.hasData){
+                  final etudiants = snapshot.data?.docs.reversed.toList();
+                  for(var etud in etudiants!){
+                    final etudiantWidget=Row(
+                      children: [
+                        Text(etud['username']),
+                        ],);
+                        etudiantWidgets.add(etudiantWidget);
+                  }
+                }
+                return Expanded(
+                  child: ListView(
+                    children: etudiantWidgets,
+                  ),
+                );
+               }
+               ),*/
+               GetProfile(_uid!,'chauffeur'),
+               //ProfileMenuWidget(title:"Log out" ,subtitle: 'goodbye',icon:Icons.logout ,onPress: (){},endIcon: false,textColor: Colors.red,),
+                ElevatedButton(
+              onPressed: () async {
+                try {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const Authentification())); // Return to the sign-in currentPage
+                } catch (e) {
+                  debugPrint('Failed to sign out: $e');
+                }
+              },
+              child: const Text('Déconnexion',style: TextStyle(color: Colors.red,),),
             ),
+            ],
           ),
+        ),
       ),
 
     );
